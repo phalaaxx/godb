34df2edbd8f1fbb83e71757bfd9547fcb3a30b51
Bozhin Zafirov <bozhin@sirius.local>
2017-02-03 14:20:57 +0200

    Fix bug in calculating hashes.

--- hash.go
+++ hash.go
@@ -4,7 +4,7 @@ package cdb
 func cdbhash(data []byte) uint32 {
 	h := uint32(5381)
 	for _, r := range data {
-		h = (h + (h << 5)) & 0xFFFFFFFF
+		h = (h + (h << 5)) ^ 0xFFFFFFFF
 		h ^= uint32(r)
 	}
 	return h
